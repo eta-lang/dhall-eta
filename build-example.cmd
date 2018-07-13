596fd79e9ca3ec543d8d337bbcceeaa318f08d7c
jneira <atreyu.bbb@gmail.com>
2018-07-13 13:58:34 +0200

    scripts to build and run the example

---
+++ build-example.cmd
@@ -0,0 +1,2 @@
+etlas clean && etlas build --enable-uberjar-mode
+javac -cp dist\build\eta-0.8.0.3\dhall-eta-0.1.0\x\dhall-eta-example\build\dhall-eta-example\dhall-eta-example.jar src\main\java\org\dhall\eta\example\Client.java
---
+++ run-example.cmd
@@ -0,0 +1,1 @@
+java -cp src\main\java;dist\build\eta-0.8.0.3\dhall-eta-0.1.0\x\dhall-eta-example\build\dhall-eta-example\dhall-eta-example.jar org.dhall.eta.example.Client
