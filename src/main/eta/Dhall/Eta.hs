c9e8ce0473223cc360e924083d204cd2f6fd2101
jneira <atreyu.bbb@gmail.com>
2018-07-16 14:40:47 +0200

    add input methods for other primitiva types

--- src/main/eta/Dhall/Eta.hs
+++ src/main/eta/Dhall/Eta.hs
@@ -4,6 +4,23 @@ import qualified Dhall
 import qualified Data.Text as Text
 import Java
 
-foreign export java "@static org.dhall.eta.Input.bool" bool
+
+input :: Dhall.Type a ->  JString -> IO a
+input ty = ( Dhall.input ty ) . Text.pack . fromJava
+
+autoInput :: Dhall.Interpret a => JString -> IO a
+autoInput = input Dhall.auto
+
+foreign export java "@static org.dhall.eta.Input.bool" autoInput
   :: JString -> IO Bool
-bool = ( Dhall.input Dhall.bool ) . Text.pack . fromJava
+
+foreign export java "@static org.dhall.eta.Input.str" autoInput
+  :: JString -> IO String
+
+foreign export java "@static org.dhall.eta.Input.integral" autoInput
+  :: JString -> IO Integer
+
+foreign export java "@static org.dhall.eta.Input.natural" natural
+  :: JString -> IO Integer
+natural = ( fmap toInteger ) . ( input Dhall.natural )
+
