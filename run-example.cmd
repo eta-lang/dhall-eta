04cd15c77eae3cd74b362e7a648baa48b9dca22f
jneira <atreyu.bbb@gmail.com>
2019-01-11 14:34:55 +0100

    Change eta version

--- run-example.cmd
+++ run-example.cmd
@@ -1,1 +1,1 @@
-java -cp examples\src\main\java;lib\dhall-eta-example;dist\build\eta-0.8.6.3\dhall-eta-0.1.0\build\dhall-eta-0.1.0-inplace.jar  org.dhall.eta.example.Client
+java -cp examples\src\main\java;lib\dhall-eta-example.jar  org.dhall.eta.example.Client
